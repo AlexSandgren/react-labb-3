ea7404448b9278b10c1011b906e17b1b6b77b669
AlexSandgren <alexsandgren98@gmail.com>
2023-11-18 01:40:15 +0100

    fixed image scaling bug

--- src/components/Pokemon/PokemonImage.tsx
+++ src/components/Pokemon/PokemonImage.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 
 function PokemonImage({ spriteInfo }: any) {
     return (
+        <div style={{width: '100%', height: '100px', position: 'relative'}}>
         <Image
+            style={{maxHeight:'100px'}}
             alt=""
             className="poke-image"
-            width="96"
-            height="96"
+            fill={true}
+            objectFit='contain'
             src={
                 (spriteInfo?.versions['generation-v']['black-white'].animated
                     .front_default
@@ -16,6 +18,7 @@ function PokemonImage({ spriteInfo }: any) {
                 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Pokeball.png/640px-Pokeball.png'
             }
         />
+        </div>
     )
 }
 
