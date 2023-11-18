24464c85f8c5ab1e6daa0ba4d635b7c72665ca44
AlexSandgren <alexsandgren98@gmail.com>
2023-11-18 01:33:11 +0100

    changed placeholder image

--- src/components/Pokemon/PokemonImage.tsx
+++ src/components/Pokemon/PokemonImage.tsx
@@ -13,7 +13,7 @@ function PokemonImage({ spriteInfo }: any) {
                     ? spriteInfo.versions['generation-v']['black-white']
                           .animated.front_default
                     : spriteInfo?.front_default) ||
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/601px-Pokebola-pokeball-png-0.png?20161126061739'
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Pokeball.png/640px-Pokeball.png'
             }
         />
     )
